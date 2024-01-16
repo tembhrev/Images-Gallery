59ab6572bb1a936f290038193b8fb2d9f1564285
tembhrev <sw.vikastembhre@gmail.com>
2024-01-16 21:57:04 +0530

    created pymongo client

--- api/main.py
+++ api/main.py
@@ -3,20 +3,21 @@ import requests
 from flask import Flask, request
 from dotenv import load_dotenv
 from flask_cors import CORS
+from mongo_client import insert_test_document
 
 load_dotenv(dotenv_path=".env.local")
 
 UNSPLASH_URL="https://api.unsplash.com/photos/random"
 UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
 DEBUG=os.environ.get("DEBUG", True)
-print(UNSPLASH_KEY)
-print(DEBUG)
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
     
 app = Flask(__name__)
 CORS(app)
+
+insert_test_document()
 
 app.config["DEBUG"] = DEBUG
 
