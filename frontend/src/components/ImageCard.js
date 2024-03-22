8002f14ffd0e64c9abb0cb71668181ec6145b4de
tembhrev <sw.vikastembhre@gmail.com>
2024-03-22 16:26:57 +0530

    add author information for image name

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Nav } from "react-bootstrap";
 
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || 'No author name';
+  const authorPortfolioURL = image.user?.portfolio_url;
+
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={image.urls.small} />
@@ -18,6 +21,14 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           </Button>
         )}
       </Card.Body>
+      <Card.Footer className="text-center text-muted">
+        {authorPortfolioURL && (
+          <Nav.Link href={authorPortfolioURL} target="_blank">
+            {authorName}
+          </Nav.Link>
+        )}
+        {!authorPortfolioURL && authorName}
+      </Card.Footer>
     </Card>
   );
 };
